ea255512c45ed044b5e3691af681ade7ae26941d
geril-ye e <passaupo20@gmail.com>
2024-08-15 09:03:50 +0000

    feat: initiated the project

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x3A47b760e186a71Ec3ce1a60Ef1Bcd804b230820'
+export const ProxyAdmin = '0x75E42b3dB31c0f5EBf40925e4E81D7f26F16f86B'
+export const SWTRImplementation = '0x6e409056Ee80E8709fe1E17e485a25c9477E48CE'
     
